7f5622e9744544ae020a608ac887ef02cdbc8868
AlanGanem <ganem.alan@hotmail.com>
2022-07-03 19:19:48 -0300

    fixed location of buid folder on local_build

--- local_build_lammps.sh
+++ local_build_lammps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Function to report errors and interrupt the script execution
 function fail() {
 	echo "ERROR: $@"
@@ -12,8 +11,8 @@ mkdir build || fail "Could not create the build directory"
 #build instrumentation
 cd utils; make || fail "Error when building time instrumentation" 
 
-cd build; cmake ../cmake/ -DCMAKE_BUILD_TYPE=Release  -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on  || \
+cd ../build; cmake ../cmake/ -DCMAKE_BUILD_TYPE=Release  -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on  || \
 	fail "Error when configuring LAMMPS build directory" 
-	
-cd build; make -j 4 || \
+
+make -j 4 || \
     fail "Error when building LAMMPS"
