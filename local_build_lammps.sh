9581c9483c99dc8c97183b7901e9585691e0b7bd
AlanGanem <ganem.alan@hotmail.com>
2022-07-03 17:27:54 -0300

    fixed typos in local_build

--- local_build_lammps.sh
+++ local_build_lammps.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-cd mo833-lammps; mkdir build || fail "Could not create the build directory"
+# Function to report errors and interrupt the script execution
+function fail() {
+	echo "ERROR: $@"
+	exit 1
+}
+
+
+mkdir build || fail "Could not create the build directory"
 
 #build instrumentation
-cd mo833-lammps/utils; make || fail "Error when building time instrumentation" 
+cd utils; make || fail "Error when building time instrumentation" 
 
-cd mo833-lammps/build; cmake ../cmake/ -DCMAKE_BUILD_TYPE=Release  -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on  || \
+cd build; cmake ../cmake/ -DCMAKE_BUILD_TYPE=Release  -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on  || \
 	fail "Error when configuring LAMMPS build directory" 
 	
-cd mo833-lammps/build; make -j 4 || \
+cd build; make -j 4 || \
+    fail "Error when building LAMMPS"
